b08bd402a6ba84ec5cfb54c9110f8acd3ff53c87
kim <beomsu9696@daum.net>
2021-03-10 14:44:32 +0900

    Initial commit.

---
+++ .history/App_20210310142226.js
@@ -0,0 +1,45 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+const App = (props) => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+        <Text style={styles.title}>Hello, World</Text>
+      </SafeAreaView>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  title: {
+    flex: 1,
+    justifyContent: 'center',,
+  },
+});
+
+export default App;
